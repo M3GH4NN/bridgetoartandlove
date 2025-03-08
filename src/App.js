f0a639221614a88d4d0ad71af450e8bf0768bd52
Meghann Cunningham <mc@meghann.dev>
2025-03-08 17:30:53 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+import Gallery from "./Gallery";
+import About from "./About";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
